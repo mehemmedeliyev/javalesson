0609bb931ba9acb5602f0efb13f874aa00c0242e
mehemmedeliyev <79990609+mehemmedeliyev@users.noreply.github.com>
2021-04-02 17:59:03 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+var yas= prompt ("Yaşınızı yazın");
+ 
+var dogumili=2020-yas;
+
+
+document.write("Sizin Doğum iliniz"+" "+ dogumili +" " + "ildir");
+
+
+
+/*  ------------------------  lesson2 ----------------*/
+
+var yas= 19;
+
+
+if (yas > 18) {
+    console.log("sizin yasiniz 18 den boyukdur")
+} else {
+    console.log("sizin yasiniz 18 den kicikdir")
+}
